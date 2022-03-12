15e70b4425067d4d8c4cd0ea57b37c066ab55683
Sauravx03 <Sauravgh123@gmail.com>
2022-03-12 09:24:48 -0800

    Added the message in listen

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ var port = process.env.port || 8080;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(port);
+}).listen(port, ()=>{
+    console.log("... port %d in %s mode", app.address().port, app.settings.env);
+});
