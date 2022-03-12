aee6538c174c9a33bc022a5c52917f6ac8be7c9b
Sauravx03 <Sauravgh123@gmail.com>
2022-03-12 09:37:53 -0800

    Updating 2 the message in listen

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var http = require('http');
 
-var port = process.env.port || 8080;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
 
-http.createServer(function (req, res) {
+var app = http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(port, ()=>{
-    console.log("... port " + port);
+});
+
+app.listen(server_port,function() {
+    console.log('Listening on  port %d',server_port);
 });
